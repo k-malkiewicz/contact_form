3162d1a6c3fcfc7b04fe2ff29a70b89232a36c91
k_malkiewicz <kamilmalkiewicz9@gmail.com>
2024-06-04 21:07:58 +0200

    Add success toast component

---
+++ src/app/components/success-toast/success-toast.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success-toast',
+  standalone: true,
+  imports: [],
+  templateUrl: './success-toast.component.html',
+  styleUrl: './success-toast.component.scss'
+})
+export class SuccessToastComponent {
+
+}
