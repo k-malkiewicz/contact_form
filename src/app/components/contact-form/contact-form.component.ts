8ed2d9db1f53128b7c761f383ff545c626b5bd77
k_malkiewicz <kamilmalkiewicz9@gmail.com>
2024-06-05 07:56:19 +0200

    Add code for contact form component

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -1,12 +1,72 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SuccessToastComponent } from "../success-toast/success-toast.component";
+import { SuccessToastService } from '../../services/success-toast.service';
 
 @Component({
-  selector: 'app-contact-form',
-  standalone: true,
-  imports: [],
-  templateUrl: './contact-form.component.html',
-  styleUrl: './contact-form.component.scss'
+    selector: 'app-contact-form',
+    standalone: true,
+    templateUrl: './contact-form.component.html',
+    styleUrl: './contact-form.component.scss',
+    imports: [ReactiveFormsModule, CommonModule, SuccessToastComponent]
 })
 export class ContactFormComponent {
+  contactForm?: any;
+  contactFormSubmitted = false;
 
+  constructor(
+    private fb: FormBuilder,
+    public successToastService: SuccessToastService
+  ) {
+    this.contactForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      emailAddress: ['', [Validators.required ,Validators.email]],
+      queryType: ['', Validators.required],
+      message: ['', Validators.required],
+      consent: ['', Validators.required]
+    }, {
+      updateOn: 'submit'
+    });
+
+    console.log(this.contactForm)
+  }
+
+  validateForm(): void {
+    if (this.contactForm.valid) {
+      this.successToastService!.isFormValid = true;
+      setTimeout(() => this.resetForm(), 2000);
+    }
+  }
+
+  resetForm() {
+    this.contactForm?.reset();
+    this.successToastService!.isFormValid = false;
+    this.contactFormSubmitted = false;
+  }
+
+  get firstName() {
+    return this.contactForm.get('firstName');
+  }
+
+  get lastName() {
+    return this.contactForm.get('lastName');
+  }
+
+  get emailAddress() {
+    return this.contactForm.get('emailAddress');
+  }
+
+  get queryType() {
+    return this.contactForm.get('queryType');
+  }
+
+  get message() {
+    return this.contactForm.get('message');
+  }
+
+  get consent() {
+    return this.contactForm.get('consent');
+  }
 }
