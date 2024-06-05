874299dc9e7929c0505e2d2d28c6104421a9e18a
k_malkiewicz <kamilmalkiewicz9@gmail.com>
2024-06-05 07:57:28 +0200

    Modify app component file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ContactFormComponent } from "./components/contact-form/contact-form.component";
+import { SuccessToastComponent } from "./components/success-toast/success-toast.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, ContactFormComponent, SuccessToastComponent]
 })
 export class AppComponent {
-  title = 'contact_form';
+  
 }
